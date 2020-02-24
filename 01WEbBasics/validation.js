f3c669aa1900db0e1a905ce468a367a15c2dbb22
javed432 <javed.akhtar432@gmail.com>
2020-02-24 20:50:26 +0530

    Web Complete

--- 01WEbBasics/validation.js
+++ 01WEbBasics/validation.js
@@ -10,3 +10,14 @@ function myValidation() {
     valid.textContent = "This input is cool";
   }
 }
+
+//form validation for reset
+document.querySelector(".myForm").addEventListener("submit", event => {
+  event.preventDefault();
+
+  console.log(event.target.username.value);
+  console.log(event.target.realname.value);
+  event.target.username.value = " ";
+  event.target.realname.value = " ";
+});
+//write a js to check two passwords and print password not same
